53e58eeb95bcd5369fe7dad49f222ede62ea6c16
Temirlan <tbazarzhangmail.com@temirlan-3.local>
2022-11-16 01:02:23 +0600

    add test files

---
+++ test/assignment_examples.py
@@ -0,0 +1,34 @@
+# Text type
+s = 'hello, world!'
+custom_str = ''.join(['a', 'b', 'c'])
+
+# Numeric types
+x = 10
+y = 3.0
+c = 3 + 6j
+
+# Sequence types
+l = [1, 2, 3, 4]
+t = (1, 2, 3, 4)
+r = range(0, 10)
+
+# Mapping type
+d = {}
+d['france'] = 'paris'
+d['spain'] = 'madrid'
+d['italy'] = 'rome'
+
+# Set types
+set_var = set()
+set_var.add(0)
+set_var.add(1)
+set_var.add(2)
+
+frozen_set = frozenset(l)
+
+# Boolean type
+bv1 = True
+bv2 = (x > y)
+
+# Nonetype
+null_var = None
