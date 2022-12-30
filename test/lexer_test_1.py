6e886f8f2e09ff65a73852cc8b428e7e08aa4daa
phoebus7 <bazarzhan.temirlan@gmail.com>
2022-12-31 00:54:28 +0600

    test modifications

---
+++ test/lexer_test_1.py
@@ -0,0 +1,24 @@
+a = []                                                                                                                           
+a = ()
+a = {}
+
+a = 1 & 2
+a = 1 | 2
+a = 1 ^ 2
+a = ~1
+
+a = 4 >> 2
+a = 4 << 2
+
+a = True
+b = False
+c = a and b
+c = a or b
+c = not a;
+
+d = [1, 2]
+res = 1 in d
+res = 0 not in d
+res = a is c
+res = a is not c
+d = d[:]
